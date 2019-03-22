49da3d5c9af1a4240881c954efcf39bb3ad57ca4
rednjzz <rednjzz@gmail.com>
2019-03-22 13:44:19 +0900

    add platcube logo and miner modify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo_platcube.png">
+    <HelloWorld msg="Welcome to PlatCube Homepage"/>
   </div>
 </template>
 
